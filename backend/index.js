b5b3b9179f8a8c2ff096e2e527c9060608ca59d2
Aman Singh Nishad <115929976+amansinghnishad@users.noreply.github.com>
2025-04-07 05:33:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 const authRoutes = require('./routes/authRoutes');
-// const serviceRequestsRoutes = require('./routes/serviceRequests');
-// const feedbackRoutes = require('./routes/feedbackRoutes'); // Import feedback routes
+const serviceRequestsRoutes = require('./routes/serviceRequests');
+const feedbackRoutes = require('./routes/feedbackRoutes'); // Import feedback routes
 const dataBase = require("./config/dataBase");
 
 dataBase.connect();
@@ -21,8 +21,8 @@ app.get('/', (req, res) => {
 //Routes
 app.use("/api/auth", authRoutes); 
 
-// app.use('/api', serviceRequestsRoutes);
-// app.use('/api/feedback', feedbackRoutes); // Add feedback routes
+app.use('/api', serviceRequestsRoutes);
+app.use('/api/feedback', feedbackRoutes); // Add feedback routes
 
 // Define PORT
 const PORT = process.env.PORT || 4000;
