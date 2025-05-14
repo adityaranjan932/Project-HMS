7947fe0367b1c5cdfc80a1c367549fde49b204c2
ASN <amansinghnishad0808@gmail.com>
2025-05-14 08:15:35 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,13 @@ const PORT = process.env.PORT || 4000; // Ensure this matches the frontend's API
 dataBase.connect();
 
 // CORS middleware
+const allowedOrigins = ['http://localhost:5173'];
+if (process.env.RENDER_FRONTEND_URL) {
+  allowedOrigins.push(process.env.RENDER_FRONTEND_URL);
+}
+
 app.use(cors({
-  origin: [
-    'http://localhost:5173', // local frontend
-    'https://project-hms-frontend-l3vz.onrender.com' // deployed frontend
-  ],
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
